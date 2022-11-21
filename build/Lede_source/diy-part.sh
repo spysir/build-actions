2a2f33f346e19360fa1881de09d282bfe45fdcbc
spysir <59646715+spysir@users.noreply.github.com>
2022-11-22 06:07:51 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -4,7 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
-git clone https://github.com/spysir/luci-app-pushbot package/luci-app-pushbot-gotify
+git clone https://github.com/spysir/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/spysir/luci-app-autotimeset.git package/luci-app-autotimeset
 
 
