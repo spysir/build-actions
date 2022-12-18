4b254a7eb97f3b8df260e4e5b3364f404bbdab51
spysir <59646715+spysir@users.noreply.github.com>
2022-12-18 14:19:40 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -5,6 +5,10 @@
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
 
+git clone https://github.com/spysir/luci-app-pushbot package/luci-app-pushbot
+git clone https://github.com/spysir/luci-app-tcpdump.git package/luci-app-tcpdump
+
+
 
 
 cat >$NETIP <<-EOF
